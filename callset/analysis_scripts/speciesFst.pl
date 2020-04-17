c0e80a363df3d948cd7e0ffdacd42450346cd3ad
tplinderoth <tylerp.linderoth@gmail.com>
2020-04-17 20:58:29 +0100

    add comptype option

--- callset/analysis_scripts/speciesFst.pl
+++ callset/analysis_scripts/speciesFst.pl
@@ -6,14 +6,19 @@ use warnings;
 use strict;
 use Getopt::Long;
 
+my $version = '1.1.0';
+
 my $spfile = undef;
 my $idfile = undef;
 my $vcf = undef;
 my $posfile = undef;
 my $passonly = undef;
 my $out = undef;
+my $comptype = 'all';
 
 die(qq/
+speciesFst.pl v $version;
+
 speciesFst.pl [input]
 
 Input:
@@ -23,9 +28,19 @@ Input:
 --passonly  Use only PASS sites from FILTER field
 --posfile   Position file with (1) chr, and (2) position of sites to analyze
 --out       Output file name prefix (required)
+--comptype  string specifying what pairwise species comparisons to make [$comptype]
 
-Fst will be calculated between all pairs in the spfile.
 Genus and species names in spfile should match those in the idfile.
+
+comptype arguments, where INT refers to the species in the INT row from the top of the spfile:
+all        all pairwise comparisons
+INT,all    pairwise comparisons between the INT species and all others
+INT,down   pairwise comparisons between the INT species and all others below it
+INT1,INT2  pairwise comparison betwen INT1 and INT2 species
+
+A set of species can be specified as '{INT1,INT2,...,INTn}' (without the single quotes) in place of INT
+to specify multiple species, in which case all species in the first set will be compared to all species 
+in the second set. 
 
 Dependencies:
 VCFtools must be installed and in user's PATH
@@ -35,7 +50,8 @@ VCFtools must be installed and in user's PATH
 die("ERROR: Could not execute vcftools --> check that executable is in PATH") if system("vcftools > /dev/null 2>&1") < 0;
 
 # parse arguments
-GetOptions('spfile=s' => \$spfile, 'idfile=s' => \$idfile, 'vcf=s' => \$vcf, 'passonly' => \$passonly, 'posfile=s' => \$posfile, 'out=s' => \$out);
+GetOptions('spfile=s' => \$spfile, 'idfile=s' => \$idfile, 'vcf=s' => \$vcf, 'passonly' => \$passonly, 
+'posfile=s' => \$posfile, 'out=s' => \$out, 'comptype=s' => \$comptype);
 
 # check that arguments are valid
 open(my $spfh, '<', $spfile) or die("Could not open species file $spfile: $!\n");
@@ -92,11 +108,55 @@ while(<$idfh>) {
 
 close $idfh;
 
+# parse type of comparison to do
+
+my (@idx1, @idx2);
+
+if ($comptype !~ /,/ && $comptype eq 'all') {
+	@idx1 = 0 .. $#sp;
+	@idx2 = 1 .. $#sp;
+} elsif ($comptype =~ /(\d+),down/) {
+	$idx1[0] = $1-1;
+	@idx2 = $1 .. $#sp;
+} elsif ($comptype =~ /^(all|\d+|\{[\d+,?]+\}),(all|\d+|\{[\d+,?]+\})$/) {
+	my $first = $1;
+	my $second = $2;
+
+	if ($first eq 'all') {
+		@idx1 = 0 .. $#sp;
+	} elsif ($first =~ /^(\d+)/) {
+		$idx1[0] = $1-1;
+	} elsif ($first =~ /^\{([\d+,?]+)\}/) {
+		@idx1 = split(/,/, $1);
+		pop @idx1 if ($idx1[$#idx1] eq ',');
+		@idx1 = map {$_-1} @idx1;
+	} else {
+		die("Unrecognized --comptype argument $comptype\n");
+	}
+
+	if ($second eq 'all') {
+		@idx2 = 0 .. $#sp;
+	} elsif ($second =~ /^(\d+)/) {
+		$idx2[0] = $1-1;
+	} elsif ($second =~ /^\{([\d+,?]+)\}/) {
+		@idx2 = split(/,/, $1);
+		pop @idx2 if ($idx2[$#idx2] eq ',');
+		@idx2 = map {$_-1} @idx2;
+	} else {
+		die("Unrecognized --comptype argument $comptype\n");
+	}
+	
+} else {
+	die("Unrecognized --comptype argument $comptype\n");
+}
+
 # process species pairs
 
+my %compared;
+my $postmp = "";
 my $fstfiles;
 
-for (my $i=0; $i < $#sp; $i++) {
+foreach my $i (@idx1) {
 
 	# make sample list 1
 	my $splist1 = "${out}_sp1_${sp[$i]}_tmp.txt";
@@ -106,7 +166,11 @@ for (my $i=0; $i < $#sp; $i++) {
 	}
 	close $spfh;
 
-	for (my $j=$i+1; $j <= $#sp; $j++) {
+	for my $j (@idx2) {
+
+		if ($j == $i || exists $compared{"${i}_${j}"} || exists $compared{"${j}_${i}"}) {
+			next;
+		}
 
 		# make sample list 2
 		my $splist2 = "${out}_sp2_${sp[$j]}_tmp.txt";
@@ -134,12 +198,12 @@ for (my $i=0; $i < $#sp; $i++) {
 		my $rv = system($vcftools_command);
 		die("ERROR: Failure running vcftools\n") if ($rv);
 
-		if ($i == 0 && $j == 1) {
+		if (!$postmp) {
 		# extract chromosome and positions
-			my $posfile = $fstout . '.pos';
-			$rv = system("cut -f1,2 $fstval > $posfile");
+			$postmp = $fstout . '.pos';
+			$rv = system("cut -f1,2 $fstval > $postmp");
 			die("ERROR: Could not extract positions from fst file $fstval\n") if ($rv);
-			$fstfiles = $posfile;
+			$fstfiles = $postmp;
 		}
 
 		# extract fst values and add header
@@ -155,6 +219,8 @@ for (my $i=0; $i < $#sp; $i++) {
 		print $outavg_fh "$sp[$i]\t$sp[$j]\t$unweighted\t$weighted\n";
 
 		unlink($splist2) or warn("WARNING: Couldn't remove temporary file $splist2");
+
+		$compared{"${i}_${j}"} = 1;
 	}
 
 	unlink($splist1) or warn("WARNING: Couldn't remove temporary file $splist1");
